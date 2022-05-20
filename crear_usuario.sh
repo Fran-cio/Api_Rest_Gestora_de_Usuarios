02ec55b68e5e1f13229272f0013a484aef898460
Francisco Ciordia <gatupancho@gmail.com>
2022-05-20 11:00:29 -0300

    feat: Eleccion de conexion

--- crear_usuario.sh
+++ crear_usuario.sh
@@ -10,4 +10,7 @@ echo -n "contrasenia nueva: "
 read -r new_pass
 new_password=$(perl -e 'print crypt($ARGV[0], "password")' $new_pass)
 json="{\"username\": \"$new_user\", \"password\": \"$new_password\"}"
-echo -n $json | http -v POST tp6.com.ar/api/users --auth $usuario:$usuario
+echo -n "Especifique la conexion o la [Ip:Port] : " 
+read -r conexion
+
+echo -n $json | http -v POST $conexion/api/users --auth $usuario:$usuario
